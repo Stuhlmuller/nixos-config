82149b713d3157b812236d0b483319c85a67f50e
Rodman <rodman@stuhlmuller.net>
2026-07-18 21:02:53 -0700

    feat: add portable Nix development shell

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Portable base development shell";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              curl
+              gh
+              git
+              gnupg
+              jq
+              nixfmt
+              ripgrep
+            ];
+          };
+        }
+      );
+    };
+}
