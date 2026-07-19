663800bd03053869ed01ecfc86799df96b8f711f
Rodman <rodman@stuhlmuller.net>
2026-07-18 22:43:45 -0700

    feat(zsh): configure Starship with Nerd Font theme

--- users/themanofrod/programs/zsh.nix
+++ users/themanofrod/programs/zsh.nix
@@ -7,6 +7,7 @@
     enableSyntaxHighlighting = true;
 
     promptInit = ''
+      export STARSHIP_CONFIG=${../../../themes/nerd-font.toml}
       eval "$(${pkgs.starship}/bin/starship init zsh)"
     '';
   };
