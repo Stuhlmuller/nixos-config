e2da7a63097739aae98479c1e7fab5604bfbbde4
Rodman <rodman@stuhlmuller.net>
2026-07-18 22:48:01 -0700

    fix: theme

--- users/themanofrod/programs/zsh.nix
+++ users/themanofrod/programs/zsh.nix
@@ -7,7 +7,6 @@
     enableSyntaxHighlighting = true;
 
     promptInit = ''
-      export STARSHIP_CONFIG=${../../../themes/nerd-font.toml}
       eval "$(${pkgs.starship}/bin/starship init zsh)"
     '';
   };
